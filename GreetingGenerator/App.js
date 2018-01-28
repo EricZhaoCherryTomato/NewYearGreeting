2e67c167573d54658da307010f826ec888cef33b
Eric Zhao <eric.ye.zhao@gmail.com>
2018-01-28 18:11:08 +1000

    WIP

--- GreetingGenerator/App.js
+++ GreetingGenerator/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput, ScrollView } from 'react-native';
 import {
   AdMobBanner,
   AdMobInterstitial,
@@ -9,48 +9,62 @@ import {
 
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isAdOpen: false,
+      senderName: "hey",
+      receviverName: "hello",
+      geetings:"新春快乐，万事如意"
+      }
+    }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-
-        
-        <AdMobBanner
-          bannerSize="fullBanner"
-          adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
-          testDeviceID="EMULATOR"
-          didFailToReceiveAdWithError={this.bannerError} />
-
-          <Button
-            onPress={() => {
-              AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // Test ID, Replace with your-admob-unit-id
-              AdMobRewarded.setTestDeviceID('EMULATOR');
-              AdMobRewarded.requestAd(() => AdMobRewarded.showAd());
-            }}
-            title="Show AdMob Rewarded"
-          />
-
-          <Button
-            onPress={() => {
-              AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // Test ID, Replace with your-admob-unit-id
-              AdMobInterstitial.setTestDeviceID('EMULATOR');
-              AdMobInterstitial.requestAd(() => AdMobInterstitial.showAd(() => Alert.alert('You tapped the button!')))
-              
-            }}
-            title="Show AdMob Interstitial"
-          />
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.textview}>
+            <TextInput
+              style={styles.text}
+              onChangeText={(senderName) => this.setState({senderName})}
+              value={this.state.senderName}
+            />
+          </View>
+          <View style={styles.textview}>
+            <TextInput
+              style={styles.text}
+              onChangeText={(receviverName) => this.setState({receviverName})}
+              value={this.state.receviverName}
+            />
+          </View>
+          <View>
+            <Button
+              onPress={() => {
+                AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // Test ID, Replace with your-admob-unit-id
+                AdMobRewarded.setTestDeviceID('EMULATOR');
+                AdMobRewarded.requestAd(() => AdMobRewarded.showAd(() => this.setState({isAdOpen:true})));
+              }}
+              title="获得专属贺词"
+            />
+             {this.state.isAdOpen ? <Text>{this.state.senderName + this.state.receviverName}</Text> : null}
+          </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    backgroundColor: '#FAFAFA',
+    
   },
-});
+  textview:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
+  },
+  text:{
+    width:300
+  }
+  });
